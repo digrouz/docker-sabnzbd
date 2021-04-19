625bdeb41eb7531ac8f5e4b861bfa40be0de7c29
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2021-04-19 13:38:58 +0200

    Update update-par2cmdline.sh

--- .github/scripts/update-par2cmdline.sh
+++ .github/scripts/update-par2cmdline.sh
@@ -5,7 +5,7 @@ PAR2_URL="https://api.github.com/repos/Parchive/par2cmdline/tags"
 
 LAST_VERSION=$(curl -SsL ${PAR2_URL} | jq .[0].name -r )
 
-sed -i -e "s|PAR2CMDLINE_VERSION='.*'|PAR2CMDLINE_VERSION='${LAST_VERSION}'|" Dockerfile_*
+sed -i -e "s|PAR2CMDLINE_VERSION='.*'|PAR2CMDLINE_VERSION='${LAST_VERSION}'|" Dockerfile*
 
 if output=$(git status --porcelain) && [ -z "$output" ]; then
   # Working directory clean
