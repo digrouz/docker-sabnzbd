fda5ba9bc0f4089007b4fa6e87d4ef33bc8904b3
Nicolas Di Gregorio <nicolas.digregorio@gmail.com>
2021-04-19 13:39:20 +0200

    Update update-par2cmdline.sh

--- .github/scripts/update-par2cmdline.sh
+++ .github/scripts/update-par2cmdline.sh
@@ -2,7 +2,6 @@
 
 PAR2_URL="https://api.github.com/repos/Parchive/par2cmdline/tags"
 
-
 LAST_VERSION=$(curl -SsL ${PAR2_URL} | jq .[0].name -r )
 
 sed -i -e "s|PAR2CMDLINE_VERSION='.*'|PAR2CMDLINE_VERSION='${LAST_VERSION}'|" Dockerfile*
